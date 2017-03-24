80825b477e8630cccf6f45a65436505fa5bf9179
George Vad <george.vad@gmail.com>
2017-03-24 17:21:35 +0200

    Adding some project files

---
+++ proj/notifier/common/src/main/java/org/example/MainCommon.java
@@ -0,0 +1,7 @@
+package org.example;
+
+/**
+ * Created by gvad on 24/03/2017.
+ */
+public class MainCommon {
+}
---
+++ proj/notifier/src/main/java/com/example/MainNotifier.java
@@ -0,0 +1,7 @@
+package com.example;
+
+/**
+ * Created by gvad on 24/03/2017.
+ */
+public class MainNotifier {
+}
