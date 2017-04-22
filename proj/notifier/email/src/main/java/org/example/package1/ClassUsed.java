e4b2f75890117caebeaec7cbcf030ba88b98a7f8
George Vad <george.vad@gmail.com>
2017-04-22 16:59:44 +0300

    more type of variables

--- proj/notifier/email/src/main/java/org/example/package1/ClassUsed.java
+++ proj/notifier/email/src/main/java/org/example/package1/ClassUsed.java
@@ -5,6 +5,21 @@ package org.example.package1;
  */
 public class ClassUsed {
 
+    //public
+    public static final String UNUSED_PUSF = "";
+    public final String UNUSED_PUF = "";
+    public String UNUSED_PU = "";
+
+    //package visibility
+    static final String UNUSED_SF = "";
+    final String UNUSED_F = "";
+    String UNUSED = "";
+
+    //protected
+    protected static final String UNUSED_PRSF = "";
+    protected final String UNUSED_PRF = "";
+    protected String UNUSED_PR = "";
+
     private static class InnerClassUnused {}
 
     private interface InnerIntefaceUnused {}
